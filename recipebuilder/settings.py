42f87955d0fa7e48d59278cf291758270c24bda0
msc2024T <mohamad.sc66@gmail.com>
2025-07-03 16:58:34 -0400

    chang db config

--- recipebuilder/settings.py
+++ recipebuilder/settings.py
@@ -104,9 +104,7 @@ DATABASES = {
         'PASSWORD': env('DB_PASSWORD'),
         'HOST': env('DB_HOST'),
         'PORT': env('DB_PORT'),
-        'OPTIONS': {
-            'sslmode': 'require',
-        },
+
     }
 }
 
